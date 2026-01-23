f4e177815c02a7db9aec4468b1fe0db89e177420
horaciolidity <horacio.dev.sol@gmail.com>
2026-01-23 14:55:45 -0300

    correxion de errores

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const HeroImage = () => {
   return (
-    <div className='flex justify-center items-center'>
+    <div className='w-full h-full flex justify-center items-center overflow-hidden bg-gradient-to-br from-slate-900 to-slate-800'>
       <img 
         src='https://imagedelivery.net/LqiWLm-3MGbYHtFuUbcBtA/119580eb-abd9-4191-b93a-f01938786700/public' 
-        alt='Hostinger Horizons' 
+        alt='Hostinger Horizons'
+        className='w-full h-full object-contain'
       />
     </div>
   );
